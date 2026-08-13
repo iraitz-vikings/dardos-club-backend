8729b511f79610b7e933800f9538290e6f79d812
iraitz-vikings <iraitz.araolaza@gmail.com>
2026-08-13 09:45:50 +0200

    Update jugadores.js

--- src/routes/jugadores.js
+++ src/routes/jugadores.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { PrismaClient } from "@prisma/client";
+import { requireAuth } from "./auth.js";
 
 const prisma = new PrismaClient();
 const router = Router();
